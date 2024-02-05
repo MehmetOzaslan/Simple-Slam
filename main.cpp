50de5095ea941e34f226e9fb838fe098d6acbf82
Mehmet Ozaslan <mozaslan@mines.edu>
2024-02-04 17:53:47 -0700

    Working pose estimation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <Eigen/Dense>
+#include <Eigen/Jacobi>
 #include <iostream>
 #include <nanoflann.hpp>
 #include <pthread.h>
@@ -6,31 +7,47 @@
 
 using namespace nanoflann;
 
+// Eigen::Matrix4d ICP(Eigen::Matrixx<num_t, n_points,3> pointcloud2){
+
+// }
+
+
 int main() {
     using num_t = double;
-    const int n_points = 1000;
+    const int n_points = 5;
     const int max_leaf = 10;
     const int DIM = 3;
     typedef KDTreeEigenMatrixAdaptor< Eigen::Matrix<num_t,n_points,3> >  my_kd_tree_t;
 
     //Create point clouds
     Eigen::Matrix<num_t, n_points,3> pointcloud1 = Eigen::MatrixXd::Random(n_points,3);
+
+    //Rotate by the above matrix, and add some random vector to the second pointcloud.
+    
     Eigen::Matrix<num_t, 3,3> rotationalMatrix = Eigen::MatrixXd(3,3);
     rotationalMatrix << 0.73842711,  0.6743333 ,  0,
                         -0.6743333 ,  0.73842711,  0.,
                         0.        ,  0.        ,  1.;
 
-    //Rotate by the above matrix.
     Eigen::Matrix<num_t, n_points,3> pointcloud2 = pointcloud1 * rotationalMatrix;
+    Eigen::RowVector3d random_offset = Eigen::RowVector3d::Random();
+    pointcloud2.rowwise() += random_offset;
+
+    //Assume that we have a function defined at this point.
+
+    //Get the centroids.
     Eigen::Vector3d centerOfMass1 = pointcloud1.colwise().mean();
     Eigen::Vector3d centerOfMass2 = pointcloud2.colwise().mean();
 
+    //Translate the point clouds so their center of mass is at the origin.
+    pointcloud1.rowwise() -= centerOfMass1.transpose();
+    pointcloud2.rowwise() -= centerOfMass2.transpose();        
+
     //Create point clouds.
     my_kd_tree_t point_cloud_index(DIM, pointcloud1, max_leaf);
-
     point_cloud_index.index_->buildIndex();
     
-
+    Eigen::MatrixXd mapped_points = pointcloud2; 
     for (int i = 0; i < pointcloud2.rows(); ++i) {
         // Query point
         num_t query_pt[DIM] = { pointcloud2(i,0), pointcloud2(i,1), pointcloud2(i,2) };
@@ -43,10 +60,34 @@ int main() {
 
         // Perform the search
         point_cloud_index.index_->findNeighbors(resultSet, &query_pt[0]);
-
-        // Output result (for example purposes, adapt as needed)
-        std::cout << "Nearest neighbor of point " << i << " is point " << ret_index
-                << " with squared L2 distance " << out_dist_sqr << std::endl;
+        //the nearest neighbor is gonna be in the ith index <-> ret_index so update pointcloud2 so that they match up properly
+        mapped_points.row(i) = pointcloud2.row(ret_index);
     }
 
+    //Now that everything has been centralized and mapped, multiply stuff out.
+    Eigen::MatrixXd W = mapped_points.transpose() * pointcloud1;
+
+    //Now do SVD.
+    // Eigen::BDCSVD<Eigen::MatrixXd> svd(W,Eigen::ComputeThinU | Eigen::ComputeThinV);
+    Eigen::JacobiSVD<Eigen::MatrixXd> svd(W,  Eigen::ComputeThinU | Eigen::ComputeThinV);
+    Eigen::MatrixXd U = svd.matrixU();
+    Eigen::MatrixXd V = svd.matrixV(); 
+
+    //Determinant part of algorithm.
+    Eigen::MatrixXd I = Eigen::MatrixXd::Identity(U.rows(), V.rows());
+    double det = (U * V.transpose()).determinant();
+    if (det < 0) {
+        I(I.rows() - 1, I.cols() - 1) = -1; // Set the bottom-right element to -1 if det is negative
+    }
+
+    Eigen::MatrixXd R = U * I * V.transpose();
+    Eigen::Vector3d t = centerOfMass1 - R * centerOfMass2;
+
+    std::cout << "Rotational Matrix:" << std::endl << R << std::endl;
+    std::cout << "Offset:" << std::endl << t << std::endl;
+
+    Eigen::MatrixXd pose(4,4);
+    pose << R, t, 0,0,0,1;
+    
+    std::cout << "Result: " << pose << std::endl;
 }
